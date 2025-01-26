9fef3027e179b6c51dac9460dcbaa52947df2f55
Pierre_T <pierre.tsiakkaros@gmail.com>
2025-01-26 21:09:34 +0100

    refactor: rename AgentMessage to ChatAgentMessage and move to chat feature directory

--- features/chat/components/ChatAgentMessage.tsx
+++ features/chat/components/ChatAgentMessage.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import { useTranslations } from "next-intl";
-import simpleLogo from "../public/simple-logo.png";
+import simpleLogo from "../../../public/simple-logo.png";
 import { extractJsonFromResponseText } from "@/utils/responseParser";
 
-interface AgentMessageProps {
+interface ChatAgentMessageProps {
   content: string;
   children?: React.ReactNode;
 }
 
-export const AgentMessage = ({ content, children }: AgentMessageProps) => {
+export const ChatAgentMessage = ({
+  content,
+  children,
+}: ChatAgentMessageProps) => {
   const t = useTranslations("Chat");
 
   return (
